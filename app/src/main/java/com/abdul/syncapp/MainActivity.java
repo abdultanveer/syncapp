c4f8959f1fcd43f9248b80fc7fa8b98c9a889d35
abdul <abdul.tanveer@gmail.com>
2021-12-13 15:12:19 +0530

    toast onclick button

--- app/src/main/java/com/abdul/syncapp/MainActivity.java
+++ app/src/main/java/com/abdul/syncapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.abdul.syncapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToastMessage(View view) {
+        //Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.message, Toast.LENGTH_SHORT).show();
+
+        add(10,20);
+    }
+
+    int add(int fnum, int snum){
+        return fnum + snum;
     }
 }
