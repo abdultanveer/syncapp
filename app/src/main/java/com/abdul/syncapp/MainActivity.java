fda70ab52bf3bc14f0c6ed41cb947e582a7c15b0
abdul <abdul.tanveer@gmail.com>
2021-12-14 12:58:42 +0530

    setting an alarm

--- app/src/main/java/com/abdul/syncapp/MainActivity.java
+++ app/src/main/java/com/abdul/syncapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -35,7 +36,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickHandler(View view) {
-        Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9845098450"));
-        startActivity(dialIntent);
+        switch (view.getId()){
+            case  R.id.btnAlarm:
+                createAlarm("ravi plz wake up",13,0);
+                break;
+            case R.id.btnDial:
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9845098450"));
+                startActivity(dialIntent);
+                break;
+        }
+
+    }
+
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
