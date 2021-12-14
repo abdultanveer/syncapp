bec043d71d76ca73d1288fe9b794d9293e3b61b3
abdul <abdul.tanveer@gmail.com>
2021-12-14 11:51:30 +0530

    getting data from edittext

--- app/src/main/java/com/abdul/syncapp/MainActivity.java
+++ app/src/main/java/com/abdul/syncapp/MainActivity.java
@@ -5,21 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nameEditText; //declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //inflation - layout inflater
+
+        nameEditText = findViewById(R.id.etName); //initialization
     }
 
     public void showToastMessage(View view) {
-        //Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
         //Toast.makeText(this, R.string.message, Toast.LENGTH_SHORT).show();
-        Intent mIntent = new Intent(this,HomeActivity.class);
-        startActivity(mIntent);
+       /* Intent mIntent = new Intent(this,HomeActivity.class);
+        startActivity(mIntent);*/
+        String name = nameEditText.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
 
     }
 
