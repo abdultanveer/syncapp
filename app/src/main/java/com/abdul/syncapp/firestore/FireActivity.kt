46d256f765b1b6fe57f5b61a9afb8e5e1da66a48
abdul <abdul.tanveer@gmail.com>
2021-12-17 16:22:48 +0530

    firestore-1

---
+++ app/src/main/java/com/abdul/syncapp/firestore/FireActivity.kt
@@ -0,0 +1,14 @@
+package com.abdul.syncapp.firestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abdul.syncapp.R
+
+class FireActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fire)
+    }
+
+    fun firestoreHandler(view: android.view.View) {}
+}
